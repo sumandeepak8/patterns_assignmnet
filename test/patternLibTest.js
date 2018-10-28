cae306df0e44d960a0f33ee4cdc75c3a59b8321d
Suman Rajput <sumanraj@in-sumanraj.local>
2018-10-28 21:04:30 +0530

    tests for triangle patterns are added

--- test/patternLibTest.js
+++ test/patternLibTest.js
@@ -4,6 +4,7 @@ const lib = require('../src/patternLib.js');
 const {genRectangle} =lib;
 const {makeFilledRectangle} = lib;
 const {makeHollowRectangle} = lib;
+const {genTriangle} = lib;
 
 // test for filled rectangle.
 let row = "*";
@@ -26,3 +27,27 @@ assert.deepEqual(genRectangle("hollow",2,2),expectedOutput);
 
 expectedOutput = ["***","* *","***"].join("\n");
 assert.deepEqual(genRectangle("hollow",3,3),expectedOutput);
+
+// test for alternating rectangle.
+expectedOutput = ["*","-"].join("\n");
+assert.deepEqual(genRectangle("alternating",1,2),expectedOutput);
+
+expectedOutput = ["***","---","***","---"].join("\n");
+assert.deepEqual(genRectangle("alternating",3,4),expectedOutput);
+
+expectedOutput = ["*"].join("\n");
+assert.deepEqual(genRectangle("alternating",1,1),expectedOutput);
+
+// test for left triangle.
+expectedOutput = ["*"].join("\n");
+assert.deepEqual(genTriangle("left",1),expectedOutput);
+
+expectedOutput = ["*  ","** ","***"].join("\n");
+assert.deepEqual(genTriangle("left",3),expectedOutput);
+
+// test for right triangle.
+expectedOutput = ["*"].join("\n");
+assert.deepEqual(genTriangle("right",1),expectedOutput);
+
+expectedOutput = ["  *"," **","***"].join("\n");
+assert.deepEqual(genTriangle("right",3),expectedOutput);
