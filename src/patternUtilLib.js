b1802bfe29edd6eac5a0f8854db299d715a7401b
Suman Rajput <sumanraj@in-sumanraj.local>
2018-10-28 16:51:59 +0530

    created a new file src/patternUtilLib.js and modified test/patternLibTest.js

---
+++ src/patternUtilLib.js
@@ -0,0 +1,17 @@
+const repeatChar = function(char,times){
+  let line = "";
+  for(let index = 0; index < times; index++){
+    line += char;
+  }
+  return line;
+}
+
+const lineGenerator = function(leftChar,middleChar,rightChar,width){
+  let middleChars = repeatChar(middleChar,width-2);
+  if(width<2)
+    rightChar ="";
+  return leftChar.concat(middleChars.concat(rightChar));
+}
+
+
+exports.lineGenerator = lineGenerator;
